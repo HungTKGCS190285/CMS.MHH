82ed4befecea7be3f3da138e1b362b8579486a22
Man <leluongminhman2112@gmail.com>
2022-02-15 14:40:30 +0700

    Register some new database

---
+++ CMS.MHH/Models/Comment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CMS.MHH.Models
+{
+    public class Comment
+    {
+        public Comment()
+        {
+            this.Date = DateTime.Now;
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public bool IsAnonymous { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+
+        public int IdeasId { get; set; }
+        public virtual Idea Ideas { get; set; }
+
+    }
+}
--- CMS.MHH/Models/Idea.cs
+++ CMS.MHH/Models/Idea.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,48 @@ namespace CMS.MHH.Models
 {
     public class Idea
     {
+        public Idea() {
+            this.Date = DateTime.Now;
+            this.Comments = new HashSet<Comment>();
+        }
+
+        public int Id { get; set; }
+
+        public int CateId { get; set; }
+
+        public string CateName { get; set; }
+
+        public virtual Category Cate { get; set; }
+
+        public string AuthorId { get; set; }
+
+        //
+        public string Author_Email { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        [Display(Name = "Is Anonymous")]
+        public bool IsAnonymous { get; set; }
+
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public byte[] Document { get; set; }
+
+        [Display(Name = "Uploaded file")]
+        public string DocumentName { get; set; }
+
+        public int View { get; set; }
+
+        public int ThumbsUp { get; set; }
+
+        public int ThumbsDown { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+        //public IEnumerable<CommentViewModel> Coms { get; set; }
+
+
     }
 }
