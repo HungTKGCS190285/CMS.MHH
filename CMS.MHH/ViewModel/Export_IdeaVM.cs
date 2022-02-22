6a0a1a1c7621918cc4c18e03f8ddd5d35b537457
Man <leluongminhman2112@gmail.com>
2022-02-22 18:29:12 +0700

    update_export idea and files

---
+++ CMS.MHH/ViewModel/Export_IdeaVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS.MHH.ViewModel
+{
+    public class Export_IdeaVM
+    {
+        public string Content { get; set; }
+
+        public string SubmissionName { get; set; }
+
+        public int SubmissionId { get; set; }
+    }
+}
