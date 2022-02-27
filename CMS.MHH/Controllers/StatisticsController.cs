f64efe6aae494da64e4fa50a9b6b379057d5d68d
Man <leluongminhman2112@gmail.com>
2022-02-27 16:54:34 +0700

    create some feature for Hưng and Hoàng implement front end

---
+++ CMS.MHH/Controllers/StatisticsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.MHH.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Exception()
+        {
+            return View();
+        }
+    }
+}
