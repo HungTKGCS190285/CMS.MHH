0e6a8ce7b3a13da11cb18bc181d31e401fa55517
Man <leluongminhman2112@gmail.com>
2022-02-27 17:00:43 +0700

    update

--- CMS.MHH/ViewModel/StatisticsVM.cs
+++ CMS.MHH/ViewModel/StatisticsVM.cs
@@ -7,5 +7,28 @@ namespace CMS.MHH.ViewModel
 {
     public class StatisticsVM
     {
+        public decimal num_idea_QA { get; set; }
+
+        public decimal num_idea_IT { get; set; }
+
+        public decimal num_idea_HR { get; set; }
+
+        public decimal per_idea_QA { get; set; }
+
+        public decimal per_idea_IT { get; set; }
+
+        public decimal per_idea_HR { get; set; }
+
+        public int num_contri_QA { get; set; }
+
+        public int num_contri_IT { get; set; }
+
+        public int num_contri_HR { get; set; }
+
+        public int idea_no_comment { get; set; }
+
+        public int idea_anonymous { get; set; }
+
+        public int comment_anonymous { get; set; }
     }
 }
